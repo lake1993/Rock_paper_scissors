633fed46eb9f76238bad630cfe718e882ef95575
lake1993 <matthew.lake.1993@gmail.com>
2022-02-28 21:41:22 +0000

    skeleton of rock paper scissors game

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function computerPlay() {
-    const selector = ["rock","paper","scissors"];
+    const selector = ["rock", "paper", "scissors"];
     const randomSelector = Math.floor(Math.random() * selector.length);
     return (selector[randomSelector]);
 }
@@ -7,67 +7,87 @@ function computerPlay() {
 let playerScore = 0;
 let computerScore = 0;
 
-function playRound(computerSelection,playerSelection) {
-            switch(true) {
-                case (computerSelection == "rock" && playerSelection == "rock"):
-                    return "Draw!"
-                    break;
-                case (computerSelection == "rock" && playerSelection == "paper"):
-                    playerScore++;
-                    return "Paper beats Rock. You Win!";
-                    break;
-                case (computerSelection == "rock" && playerSelection == "scissors"):
-                    computerScore++;
-                    return "Rock beats Scissors! You Lose!";
-                    break;
-                case (computerSelection == "paper" && playerSelection == "rock"):
-                    computerScore++;
-                    return "Paper beats Rock! You Lose!";
-                    break;
-                case (computerSelection == "paper" && playerSelection == "paper"):
-                    return "Draw!";
-                    break;
-                case (computerSelection == "paper" && playerSelection == "scissors"):
-                    playerScore++;
-                    return "Scissors beats Paper! You Win!";
-                    break;
-                case (computerSelection == "scissors" && playerSelection == "rock"):
-                    playerScore++;
-                    return "Rock beats Scissors! You Win!";
-                    break;
-                case (computerSelection == "scissors" && playerSelection == "paper"):
-                    computerScore++;
-                    return "Scissors beats Paper! You Lose!";
-                    break;
-                case (computerSelection == "scissors" && playerSelection == "scissors"):
-                    return "Draw!";
-                    break;
-                default:
-                    return "Not a valid answer!";
-                    break;
-                
-            }
-        }
+function playRound(computerSelection, playerSelection) {
+    switch (true) {
+        case (computerSelection == "rock" && playerSelection == "rock"):
 
+            return "Draw!"
+            break;
+        case (computerSelection == "rock" && playerSelection == "paper"):
+            playerScore++;
+            return "Paper beats Rock. You Win!";
+            break;
+        case (computerSelection == "rock" && playerSelection == "scissors"):
+            computerScore++;
+            return "Rock beats Scissors! You Lose!";
+            break;
+        case (computerSelection == "paper" && playerSelection == "rock"):
+            computerScore++;
+            return "Paper beats Rock! You Lose!";
+            break;
+        case (computerSelection == "paper" && playerSelection == "paper"):
+            return "Draw!";
+            break;
+        case (computerSelection == "paper" && playerSelection == "scissors"):
+            playerScore++;
+            return "Scissors beats Paper! You Win!";
+            break;
+        case (computerSelection == "scissors" && playerSelection == "rock"):
+            playerScore++;
+            return "Rock beats Scissors! You Win!";
+            break;
+        case (computerSelection == "scissors" && playerSelection == "paper"):
+            computerScore++;
+            return "Scissors beats Paper! You Lose!";
+            break;
+        case (computerSelection == "scissors" && playerSelection == "scissors"):
+            return "Draw!";
+            break;
+        default:
+            return "Not a valid answer!";
+            break;
 
-function game () {
-     while (playerScore < 5 && computerScore < 5) {
-        const computerSel = computerPlay();
-        let playerSel = prompt("Rock, Paper, Scissors?");
-        let playerSelLower = playerSel.toLowerCase();
-        console.log(playRound(computerSel,playerSelLower));
-        console.log(`Your Score is ${playerScore}.`, `    Computer's Score is ${computerScore}`)
+    }
+}
+
+function result(playerSel) {
+    const computerSel = computerPlay();
+            let roundResult = playRound(computerSel, playerSel);
+            document.querySelector('.roundResult').textContent = roundResult;
+            document.querySelector('.playerScore').textContent = playerScore;
+            document.querySelector('.computerScore').textContent = computerScore;
+            console.log(`Your Score is ${playerScore}.`, `    Computer's Score is ${computerScore}`)
+}
+
+function game() {
+
+    
+
+        document.querySelector('.rock').addEventListener('click', function() {
+            let playerSel = "rock";
+            result(playerSel);
         
-        }
+    })
 
-        if (computerScore == 5) {
-            alert("You Lose!");
-        }
+        document.querySelector('.paper').addEventListener('click', function() {
+            let playerSel = "paper";
+            result(playerSel);
+            
+        })
 
-        if (playerScore == 5) {
-            alert("You Win!");
-        }
-     }
+        document.querySelector('.scissors').addEventListener('click', function() {
+            let playerSel = "scissors";
+            result(playerSel);
+        })
 
-           
+    if (computerScore == 5) {
+        alert("You Lose!");
+    }
+
+    if (playerScore == 5) {
+        alert("You Win!");
+    }
+}
+
 game();
+
