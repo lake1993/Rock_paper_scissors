235da4b5c47a6e49a8b64c96685d33d412ab33d1
lake1993 <matthew.lake.1993@gmail.com>
2022-03-01 21:54:33 +0000

    added some style

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-function computerPlay() {
+
+function computerRound() {
     const selector = ["rock", "paper", "scissors"];
     const randomSelector = Math.floor(Math.random() * selector.length);
     return (selector[randomSelector]);
@@ -10,37 +11,54 @@ let computerScore = 0;
 function playRound(computerSelection, playerSelection) {
     switch (true) {
         case (computerSelection == "rock" && playerSelection == "rock"):
-
-            return "Draw!"
+            drawRound("roundResult");
+            scoreBorder();
+            return "Draw!";
             break;
         case (computerSelection == "rock" && playerSelection == "paper"):
             playerScore++;
+            scoreBorder();
+            winRound("roundResult");
             return "Paper beats Rock. You Win!";
             break;
         case (computerSelection == "rock" && playerSelection == "scissors"):
             computerScore++;
+            scoreBorder();
+            loseRound("roundResult");
             return "Rock beats Scissors! You Lose!";
             break;
         case (computerSelection == "paper" && playerSelection == "rock"):
             computerScore++;
+            scoreBorder();
+            loseRound("roundResult");
             return "Paper beats Rock! You Lose!";
             break;
         case (computerSelection == "paper" && playerSelection == "paper"):
+            drawRound("roundResult");
+            scoreBorder();
             return "Draw!";
             break;
         case (computerSelection == "paper" && playerSelection == "scissors"):
             playerScore++;
+            scoreBorder();
+            winRound("roundResult");
             return "Scissors beats Paper! You Win!";
             break;
         case (computerSelection == "scissors" && playerSelection == "rock"):
             playerScore++;
+            scoreBorder();
+            winRound("roundResult");
             return "Rock beats Scissors! You Win!";
             break;
         case (computerSelection == "scissors" && playerSelection == "paper"):
             computerScore++;
+            scoreBorder();
+            loseRound("roundResult");
             return "Scissors beats Paper! You Lose!";
             break;
         case (computerSelection == "scissors" && playerSelection == "scissors"):
+            drawRound("roundResult");
+            scoreBorder();
             return "Draw!";
             break;
         default:
@@ -48,46 +66,68 @@ function playRound(computerSelection, playerSelection) {
             break;
 
     }
+    
 }
 
-function result(playerSel) {
-    const computerSel = computerPlay();
-            let roundResult = playRound(computerSel, playerSel);
+function result(playerSelection) {
+    const computerSelection = computerRound();
+            let roundResult = playRound(computerSelection, playerSelection);
             document.querySelector('.roundResult').textContent = roundResult;
             document.querySelector('.playerScore').textContent = playerScore;
             document.querySelector('.computerScore').textContent = computerScore;
-            console.log(`Your Score is ${playerScore}.`, `    Computer's Score is ${computerScore}`)
 }
 
 function game() {
-
-    
-
         document.querySelector('.rock').addEventListener('click', function() {
-            let playerSel = "rock";
-            result(playerSel);
-        
-    })
-
+            let playerSelection = "rock";
+            result(playerSelection);
+        })
         document.querySelector('.paper').addEventListener('click', function() {
-            let playerSel = "paper";
-            result(playerSel);
+            let playerSelection = "paper";
+            result(playerSelection);
             
         })
-
         document.querySelector('.scissors').addEventListener('click', function() {
-            let playerSel = "scissors";
-            result(playerSel);
+            let playerSelection = "scissors";
+            result(playerSelection);
         })
-
-    if (computerScore == 5) {
-        alert("You Lose!");
+        
     }
 
-    if (playerScore == 5) {
-        alert("You Win!");
-    }
+function scoreBorder() {
+    if (playerScore > computerScore) {
+        winRound("yourscore")
+        loseRound("compscore")
+    } else if (playerScore == computerScore) {
+        drawRound("yourscore") 
+        drawRound("compscore")
+    } else {
+        loseRound("yourscore")
+        winRound("compscore")
+    } 
 }
+
+
+
+
+function winRound(changeBorder) {
+    document.querySelector(`.${changeBorder}`).classList.add('win');
+    document.querySelector(`.${changeBorder}`).classList.remove('lose');
+    document.querySelector(`.${changeBorder}`).classList.remove('draw');
+}
+
+function drawRound(changeBorder) {
+    document.querySelector(`.${changeBorder}`).classList.add('draw');
+    document.querySelector(`.${changeBorder}`).classList.remove('win');
+    document.querySelector(`.${changeBorder}`).classList.remove('lose');
+}
+
+function loseRound(changeBorder) {
+    document.querySelector(`.${changeBorder}`).classList.add('lose');
+    document.querySelector(`.${changeBorder}`).classList.remove('win');
+    document.querySelector(`.${changeBorder}`).classList.remove('draw');
+}
+
 
 game();
 
